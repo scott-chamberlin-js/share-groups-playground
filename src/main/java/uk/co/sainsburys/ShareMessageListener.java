b2f722051f7f57bcfb899bc3a2411cdceb14a30b
Scott Chamberlin-Wibbeke <scott.chamberlin-wib@sainsburys.co.uk>
2026-05-06 12:12:54 +0100

    Upgrade to Kafka 4.1

--- src/main/java/uk/co/sainsburys/ShareMessageListener.java
+++ src/main/java/uk/co/sainsburys/ShareMessageListener.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.ShareAcknowledgment;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -20,17 +19,8 @@ public class ShareMessageListener {
             groupId = "${kafka.thing.group-id}",
             concurrency = "2"
     )
-    public void listen(ConsumerRecord<String, String> consumerRecord, ShareAcknowledgment acknowledgment) {
-        try {
-            log.info("Received record with key={} and deliveryCount={}", consumerRecord.key(), consumerRecord.deliveryCount().orElse((short) 0));
-            messageProcessor.processMessage(consumerRecord.value());
-            acknowledgment.acknowledge(); // ACCEPT
-        } catch (RetryableException e) {
-            log.warn("Retryable exception processing record with key={}. Releasing record so it can be retried. {}", consumerRecord.key(), e.getMessage());
-            acknowledgment.release(); // RELEASE: will be redelivered
-        } catch (Exception e) {
-            log.error("Unknown error processing record. Rejecting record with key={}", consumerRecord.key(), e);
-            acknowledgment.reject(); // REJECT: permanent failure
-        }
+    public void listen(ConsumerRecord<String, String> consumerRecord) throws RetryableException {
+        log.info("Received record with key={} and deliveryCount={}", consumerRecord.key(), consumerRecord.deliveryCount().orElse((short) 0));
+        messageProcessor.processMessage(consumerRecord.value());
     }
 }
